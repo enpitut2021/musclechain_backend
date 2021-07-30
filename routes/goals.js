1ec6a87cf2c1233e75e57ee294fdb742579585e9
srfwt <water.tree2000.14@gmail.com>
2021-07-30 11:59:58 +0900

    目標立てられるよ♡

--- routes/goals.js
+++ routes/goals.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 var fs = require('fs');
 var path = require('path');
@@ -13,13 +23,26 @@ var DB = JSON.parse(
 app.get('/goals', (req, res) => {
     const ret_goal = Number(DB.goal);
     const ret_id = DB.user_id;
-    
+
     let ret = {
         user_id: ret_id,
         ret_goal: ret_goal
     };
     console.log(ret);
     res.json(ret);
+})
+
+
+app.post('/goals', function(req, res){
+    let new_goal = req.body.goals;
+    let result = DB;
+    result.goal = new_goal;
+    fs.writeFileSync(
+        path.resolve(__dirname, "../data.json"),
+        JSON.stringify(result)
+    )
+
+    res.send('POST request to the homepage');
 })
 
 app.listen(3001, () => console.log('Listening on port 3001'));
--- routes/test_load.js
+++ routes/test_load.js
@@ -3,8 +3,14 @@ var path = require('path');
 
 var data = JSON.parse( 
   fs.readFileSync( 
-    path.resolve( __dirname , '../data.json')
+    path.resolve( __dirname , '../data_copy.json')
   ) 
 );
 
 console.log(data.goal);
+var result = data;
+result.goal = 4000;
+fs.writeFileSync(
+  path.resolve(__dirname, "../data_copy.json"),
+  JSON.stringify(result)
+);
