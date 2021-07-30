1c71aeabdc13e964d866cbdab93e6bf2e3a15166
SkyLumonde <ydkbb1245@gmail.com>
2021-07-30 12:13:31 +0900

    routingを設定しました

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const calories = require('./routes/calories');
-//const goals = require('./routes/goal');
+const goals = require('./routes/goals');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.use('/calories', calories);
-//app.use('/goal', goals);
-
-app.get('/',(res,req)=>{
-    res.send('Received GET request');
-})
+app.use('/goals', goals);
 
 app.listen(3003, () => console.log('Listening on port 3003'));
---
+++ routes/goals.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const router = express.Router();
+
+var fs = require('fs');
+var path = require('path');
+
+var DB = JSON.parse( 
+    fs.readFileSync( 
+      path.resolve( __dirname , "../data.json" ) 
+    ) 
+  );
+
+router.get('/', (req, res) => {
+    const ret_goal = Number(DB.goal);
+    const ret_id = DB.user_id;
+
+    let ret = {
+        user_id: ret_id,
+        ret_goal: ret_goal
+    };
+    console.log(ret);
+    res.json(ret);
+})
+
+
+router.post('/', function(req, res){
+    let new_goal = req.body.goal;
+    let result = DB;
+    result.goal = new_goal;
+    fs.writeFileSync(
+        path.resolve(__dirname, "../data.json"),
+        JSON.stringify(result)
+    )
+
+    res.send('POST request to the homepage');
+})
+
+
+module.exports = router;
