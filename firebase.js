a61479dd91adf03ea332cecd018be8b474d15ab8
keimoriyama <keischwiiz@gmail.com>
2021-08-02 11:41:15 +0900

    関数化

--- firebase.js
+++ firebase.js
@@ -1,32 +1,34 @@
 const firebase = require("firebase");
-const admin = require("firebase-admin");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCigg4A1qraKIRlL-8NZ2ueZLz3bRJxxHc",
-  authDomain: "enpit-5b754.firebaseapp.com",
-  projectId: "enpit-5b754",
-  storageBucket: "enpit-5b754.appspot.com",
-  messagingSenderId: "74928683689",
-  appId: "1:74928683689:web:c7b45d86867e73f78607b8",
-};
+//ここから
+function firebase_init() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCigg4A1qraKIRlL-8NZ2ueZLz3bRJxxHc",
+    authDomain: "enpit-5b754.firebaseapp.com",
+    projectId: "enpit-5b754",
+    storageBucket: "enpit-5b754.appspot.com",
+    messagingSenderId: "74928683689",
+    appId: "1:74928683689:web:c7b45d86867e73f78607b8",
+  };
 
-firebase.initializeApp(firebaseConfig);
-// const serviceAccount = require("./firebase_keys/enpit-5b754-firebase-adminsdk-qlsr0-6f2e587d82.json");
+  firebase.initializeApp(firebaseConfig);
+}
+//ここまででfirebaseの認証をするので，使う時は呼び出さないと行けない
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
 var email = "hoge@example.com";
 var password = "password";
-firebase
-  .auth()
-  .createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    var user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMsg = error.message;
-    console.log(errorCode, errorMsg);
-  });
+//ユーザー認証の関数
+function add_user(email, password) {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMsg = error.message;
+      console.log(errorCode, errorMsg);
+    });
+}
