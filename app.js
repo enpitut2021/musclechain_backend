bc148a976b9ea177e8597035e35249cdf6cffb11
srfwt <water.tree2000.14@gmail.com>
2021-08-02 13:15:18 +0900

    firebase-adminの追加

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const calories = require('./routes/calories');
 const goals = require('./routes/goals');
+const rooms = require('./routes/rooms');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use('/calories', calories);
+app.use('/rooms', rooms);
 app.use('/goals', goals);
 
 app.listen(3003, () => console.log('Listening on port 3003'));
---
+++ routes/testFirebase.js
@@ -0,0 +1,6 @@
+const firebase = require("firebase");
+require("firebase/firestore");
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
