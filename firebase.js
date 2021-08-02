2f47df82d20791fa52c1c8384b4ffca566fc5868
keimoriyama <keischwiiz@gmail.com>
2021-08-02 11:15:47 +0900

    add firebase application

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebase = require("firebase");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCigg4A1qraKIRlL-8NZ2ueZLz3bRJxxHc",
+  authDomain: "enpit-5b754.firebaseapp.com",
+  projectId: "enpit-5b754",
+  storageBucket: "enpit-5b754.appspot.com",
+  messagingSenderId: "74928683689",
+  appId: "1:74928683689:web:c7b45d86867e73f78607b8",
+};
+
+firebase.initializeApp(firebaseConfig);
